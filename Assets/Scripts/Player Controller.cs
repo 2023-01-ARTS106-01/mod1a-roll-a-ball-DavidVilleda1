84ee1518c242ee4e2b95117b9c41abaa2ce70762
DavidVilleda1 <123001555+DavidVilleda1@users.noreply.github.com>
2023-01-20 18:55:21 -0500

    added script up to apply input data to player

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+Private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponets<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    Void OnMove(InputValue movementValue)
     {
-        
+
+    Vector2 movementVector = movementValue.Get<Vector2>();
+
     }
+
+
+    void FixedUpdate()
+    {
+
+
+
+    }
+
+
+
 }
